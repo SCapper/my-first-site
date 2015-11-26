47c8dda64b255ea9846bbd704a5a37343e68d018
Sean Capper <seanc1028@aol.com>
2015-11-26 00:11:50 -0500

    Changed the way posts are ordered.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.edit import CreateView
+from django.contrib.auth.forms import UserCreationForm
 
 urlpatterns = [
     # Examples:
@@ -7,5 +9,10 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
-    url(r'', include('blog.urls')),
+    url(r'^register/', CreateView.as_view(
+                        template_name='register.html',
+                        form_class=UserCreationForm,
+                        success_url='/')
+    ),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
 ]
